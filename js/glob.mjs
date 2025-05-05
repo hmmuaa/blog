7a011622418fc433f4925b96a78385dc7c74ee7a
欸欸 <33324@qq.com>
2025-05-06 00:48:07 +0800

    +

--- js/glob.mjs
+++ js/glob.mjs
@@ -1,10 +1,11 @@
+import assert,{deepEqual as eq,notDeepEqual as neq
+	,throws,rejects}from'assert/strict'
 const p=(...a)=>(console.log(...a),a[1]?a:a[0])
 ,proba=a=>Math.random()<a
 ,randomIn=(min,max)=>Math.random()*(max-min)+ +(min)
 import * as date from'./date.mjs'
-import assert,{deepEqual as eq,notDeepEqual as neq
-	,throws}from'assert/strict'
-Object.assign(global,{p,assert,eq,neq,throws,randomIn,...date})
+Object.assign(global,{p,assert,eq,neq,throws,rejects
+	,randomIn,...date})
 
 const groupBy=(a,f)=>a.reduce((a,b,i,l,__,k=f(b,i,l))=>(
 	a[k]=[...(a[k]||[]),b],a),{})
--- js/lab/Auto.js Pro/accessibility.mjs
+++
@@ -1,8 +0,0 @@
-import'#glob'
-import acc,{select as _select}from'accessibility'
-import{device}from'device'
-	const{widthPixels:w,heightPixels:h}=device.displayMetrics
-	p(w)
-	const swipe=(x1,y1,x2,y2,d=2)=>
-		acc.swipe(x1*w,y1*h,x2*w,y2*h,d*1e3)
-	await swipe(.5,.8,.5,.9)//,await swipe(.5,.9,.5,.8,.1)
