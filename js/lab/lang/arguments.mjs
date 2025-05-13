9b283a7131a7a3820b5a26ceea25c3532a0807a8
欸欸 <33324@qq.com>
2025-05-13 17:37:09 +0800

    moves

--- js/lab/lang/arguments.mjs
+++
@@ -1,6 +0,0 @@
-import'#lab'
-let f=(a,b)=>a+b;eq(f(12),NaN)
-f=(a,{b})=>a+b;throws(()=>f(12),TypeError)
-f=(a,{b}={})=>a+b;eq(f(12),NaN)
-f=(a,{b}=c)=>a+b;eq(((c={b:34})=>f(12))(),36)
-///更多见[destructure]
--- js/lab/lang/parameter.mjs
+++
@@ -1,3 +0,0 @@
-import'#glob'
-let f=(a,b=a*2)=>a+b
-eq(f(1),3)
